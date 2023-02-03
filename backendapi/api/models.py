c10ecaaa23ae946f88e28a1578c46080c04f0b56
rohan <rohankhurana77@gmail.com>
2023-02-03 16:01:36 +0530

    to-do app api using function based api view

--- backendapi/api/models.py
+++ backendapi/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    task = models.CharField(max_length=200)
+    is_completed = models.BooleanField(default=False)
--- backendapi/api/views.py
+++ backendapi/api/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def task_list(request):
+    """
+    List all the tasks, or create a new task.
+    """
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def task_detail(request, pk):
+    """
+    Retrieve, update or delete a task.
+    """
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
